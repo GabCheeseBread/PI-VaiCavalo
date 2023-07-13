692eb63a261cb8bdf670aa179918a7ce61a037a6
GabCheeseBread <127157972+GabCheeseBread@users.noreply.github.com>
2023-07-13 02:41:48 -0300

    Adicionada funcionalidade com banco de dados SQL

--- pages/cadastro_bd.php
+++ pages/cadastro_bd.php
@@ -1,15 +1,38 @@
 <?php
 	if (!isset($_POST["email"]) || !isset($_POST["nome"]) || !isset($_POST["senha"]) || !isset($_POST["c_senha"])) {
 		echo "Erro no envio de credenciais.";
-		return;
+		die();
 	}
 	$email = $_POST["email"];
 	$nome = $_POST["nome"];
 	$senha = $_POST["senha"];
 	$c_senha = $_POST["c_senha"];
 	if ($senha != $c_senha) {
+		echo $senha;
+		echo $c_senha;
 		echo "Erro na confirmação de senha.";
-		return;
+		die();
 	}
-	echo "Sucesso!";
+
+	$conn = new mysqli("localhost","root","","vc_agenda");
+	if ($conn->connect_errno) {
+		die($conn->connect_error);
+	}
+
+	$q = $conn->query("SELECT * FROM usuarios");
+
+	for ($i = 0; $i < $q->num_rows; $i++) {
+		$linha = $q->fetch_assoc();
+		if ($linha["email"] == $email) {
+			echo "Uma conta com este e-mail já existe!";
+			die();
+		}
+	}
+
+	//admito que não sei se esse === TRUE é necessário
+	if ($conn->query("INSERT INTO usuarios VALUES (NULL,'$email','$nome','$senha')") === TRUE) {
+		echo "Sucesso!";
+	} else {
+		echo "Erro com o banco de dados.";
+	}
 ?>
